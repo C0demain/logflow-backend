d380d731174bc74ae4ddb9f2a70dd708b17bdf72
Bruno Vieira <76965972+BrunoVieira003@users.noreply.github.com>
2024-09-24 16:30:53 -0300

    fix(task): muda rota base das requisições para padrão da api

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -2,8 +2,10 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Put, Query } from '@
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller('task')
+@Controller('/api/v1/task')
+@ApiTags('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
