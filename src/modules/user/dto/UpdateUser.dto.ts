7774d0222bab9055b547f526766a5878d1d27ad7
Bruno Vieira <76965972+BrunoVieira003@users.noreply.github.com>
2024-09-28 10:37:31 -0300

    fix(user): remove exigência de email único no UpdateUserDTO

--- src/modules/user/dto/UpdateUser.dto.ts
+++ src/modules/user/dto/UpdateUser.dto.ts
@@ -1,4 +1,28 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateUserDTO } from "./CreateUser.dto";
+import { IsNotEmpty, IsEmail, MinLength, IsOptional, IsEnum, IsBoolean } from "class-validator";
+import { Role } from "src/modules/roles/enums/roles.enum";
+import { Sector } from "src/modules/service-order/enums/sector.enum";
+import { UniqueEmail } from "src/modules/user/validation/UniqueEmail.validation";
 
-export class UpdateUserDTO extends PartialType(CreateUserDTO) {}
+export class UpdateUserDTO{
+    @IsNotEmpty({ message: 'O campo `name` não pode ser vazio.' })
+    name: string;
+
+    @IsEmail(undefined, {message: 'O campo `email` informado é inválido ou não foi informado.',})
+    email: string;
+
+    @IsOptional()
+    @IsEnum(Role, {message: `O campo 'role' precisa estar dentro dos padrôes estabelecidos. (${Role.EMPLOYEE},${Role.MANAGER})`,})
+    role: Role;
+
+    @IsOptional()
+    @MinLength(6, {message: 'O campo `password` precisa ter pelo menos 6 caracteres.',})
+    password: string;
+
+    @IsOptional()
+    @IsEnum(Sector, {message: 'O campo `sector` precisa estar dentro dos padrôes estabelecidos.',})
+    sector: Sector;
+
+    @IsOptional()
+    @IsBoolean({ message: 'O campo `isActive` precisa ser `true` ou `false`.' })
+    isActive: boolean;
+}
