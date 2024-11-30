ad41f571d96db8337fd15027cf7ea2b6d388dc8e
ryan-wakugawa <ryan.wakugawa@fatec.sp.gov.br>
2024-11-29 21:31:48 -0300

    teste: adiciona providers dos testes da agenda

--- src/modules/calendar/calendar.controller.spec.ts
+++ src/modules/calendar/calendar.controller.spec.ts
@@ -1,17 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CalendarController } from './calendar.controller';
+import { CalendarService } from './calendar.service';
+import { mock } from 'node:test';
+import { AuthenticationGuard } from '../auth/authentication.guard';
+
+const mockCalendarService = {
+  getEvents: jest.fn(),
+  addEvent: jest.fn(),
+  addTaskAsEvent: jest.fn(),
+  handleOAuthCallback: jest.fn(),
+};
 
 describe('CalendarController', () => {
   let controller: CalendarController;
+  let service: CalendarService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CalendarController],
-    }).compile();
+      providers: [
+        {
+          provide: CalendarService,
+          useValue: mockCalendarService,
+        },
+      ],
+    })
+      .overrideGuard(AuthenticationGuard)
+      .useValue({ canActivate: jest.fn(() => true) })
+      .compile();
 
     controller = module.get<CalendarController>(CalendarController);
+    service = module.get<CalendarService>(CalendarService);
   });
-
+  
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
--- src/modules/calendar/calendar.service.spec.ts
+++ src/modules/calendar/calendar.service.spec.ts
@@ -1,12 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CalendarService } from './calendar.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserEntity } from '../user/entities/user.entity';
+import { Task } from '../task/entities/task.entity';
+import { ServiceOrder } from '../service-order/entities/service-order.entity';
+
+const mockUserRepository = {
+  findById: jest.fn(),
+};
+const mockTaskRepository = {
+  findById: jest.fn(),
+};
+const mockServiceOrderRepository = {
+  save: jest.fn(),
+  findOne: jest.fn(),
+  find: jest.fn(),
+  delete: jest.fn(),
+};
 
 describe('CalendarService', () => {
   let service: CalendarService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CalendarService],
+      providers: [
+        CalendarService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: mockUserRepository,
+        },
+        {
+          provide: getRepositoryToken(Task),
+          useValue: mockTaskRepository,
+        },
+        {
+          provide: getRepositoryToken(ServiceOrder),
+          useValue: mockServiceOrderRepository,
+        }
+      ],
     }).compile();
 
     service = module.get<CalendarService>(CalendarService);
